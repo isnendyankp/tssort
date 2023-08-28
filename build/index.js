6d139e5ebfe11d7a94761e4a82c08e3c2b67a965
isnendyankp <isnendyankp@gmail.com>
2023-08-28 16:20:18 +0700

    S10-79:configure tsconfig rootDir & outDir

--- build/index.js
+++ build/index.js
@@ -1,5 +1,6 @@
+"use strict";
 console.log('hi there');
-
-
-// - cr8 index js by using terminal: tsc index.ts
-// - S10-79:Organize file to inside build
+// progres:
+// - S10-79:cr8 index js by using terminal: tsc index.ts
+// - S10-79:Organize file to inside src
+// - S10-79:configure tsconfig rootDir & outDir
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ console.log('hi there');
 
 // progres:
 // - S10-79:Organize file to inside src
+// - S10-79:configure tsconfig rootDir & outDir
