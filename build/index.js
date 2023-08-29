2b66471e0df28582cd07e93ab814d42ef783468b
isnendyankp <isnendyankp@gmail.com>
2023-08-29 15:56:58 +0700

    S10-80:Add call logSomething

--- build/index.js
+++ build/index.js
@@ -3,8 +3,10 @@ console.log('hi there');
 var logSomething = function () {
     console.log('something');
 };
+logSomething();
 // progres:
 // - S10-79:cr8 index js by using terminal: tsc index.ts
 // - S10-79:Organize file 
 // - S10-79:configure tsconfig rootDir & outDir
 // - S10-79:cr8 another function for make sure continue tsc -w run correctly
+// - 
