3c190108ff3e59bbc70597966939601b2645caae
isnendyankp <isnendyankp@gmail.com>
2023-09-03 07:54:19 +0700

    S10-82:Cr8 base class Sorter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-console.log('hi there');
+// console.log('123');
 
-var logSomething = function () {
-  console.log('something');
-};
-logSomething();
+// var logSomething = function () {
+//   console.log('something');
+// };
+// logSomething();
+
+class Sorter {
+
+}
+
 // progres:
 // - S10-79:cr8 index js by using terminal: tsc index.ts
 // - S10-79:Organize file 
 // - S10-79:configure tsconfig rootDir & outDir
 // - S10-79:cr8 another function for make sure continue tsc -w run correctly
-// - 
+// - S10-82:Cr8 base class Sorter
