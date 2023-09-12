a2a474f05eed16223c585d243f00d97c9204c9f0
isnendyankp <isnendyankp@gmail.com>
2023-09-12 17:10:13 +0700

    S10-85:Add new type string for collection

--- build/index.js
+++ build/index.js
@@ -1,9 +1,4 @@
 "use strict";
-// console.log('123');
-// var logSomething = function () {
-//   console.log('something');
-// };
-// logSomething();
 class Sorter {
     constructor(collection) {
         this.collection = collection;
@@ -12,11 +7,13 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
+                // if collection is an array of numbers
                 if (this.collection[j] > this.collection[j + 1]) {
                     const leftHand = this.collection[j];
                     this.collection[j] = this.collection[j + 1];
                     this.collection[j + 1] = leftHand;
                 }
+                // if collection is a string, do this logic instead: logic to compare & swap character in string
             }
         }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
-// console.log('123');
-
-// var logSomething = function () {
-//   console.log('something');
-// };
-// logSomething();
-
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // if collection is an array of numbers
         if (this.collection[j] > this.collection[j + 1]) {
           const leftHand = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = leftHand;
         }
+        // if collection is a string, do this logic instead: logic to compare & swap character in string
       }
     }
-
   }
 }
 
