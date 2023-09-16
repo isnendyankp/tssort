efce211789e53530d456c6b5789d493ee508f4ca
isnendyankp <isnendyankp@gmail.com>
2023-09-16 12:43:06 +0700

    S10-88:remove type guard on first loop logic

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -6,16 +6,11 @@ export class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        // All of this only works if collection is number[]
-        // if collection is an array of numbers
-        if (this.collection instanceof Array) {
-          // collection === number[]
           if (this.collection[j] > this.collection[j + 1]) {
             const leftHand = this.collection[j];
             this.collection[j] = this.collection[j + 1];
             this.collection[j + 1] = leftHand;
           }
-        }
         // only going to work if collection is a string
         // if collection is a string, do this logic instead: logic to compare & swap character in string
         if (typeof this.collection === 'string') {
@@ -29,3 +24,4 @@ export class Sorter {
 // - S10-88:Cr8 Sorter.ts file & move all code sorter
 // - S10-88:export class sorter
 // - S10-88:remove all type annotations
+// - S10-88:remove type guard on first loop logic & remove comment
