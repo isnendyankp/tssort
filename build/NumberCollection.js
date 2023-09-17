22c7372398ef7187fabb5a6324e319973aea5b4a
isnendyankp <isnendyankp@gmail.com>
2023-09-17 18:24:26 +0700

    S10-89:cr8 base constructor @NumberCollection class

--- build/NumberCollection.js
+++ build/NumberCollection.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NumbersCollection = void 0;
 class NumbersCollection {
+    constructor() {
+    }
 }
 exports.NumbersCollection = NumbersCollection;
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
+// - S10-89:cr8 base constructor @NumberCollection class
--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -1,6 +1,11 @@
 export class NumbersCollection {
     data: number[]
+
+    constructor() {
+
+    }
 }
 
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
+// - S10-89:cr8 base constructor @NumberCollection class
