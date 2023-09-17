58a6cf307942d15ab89ae52f5ac3602532e47754
isnendyankp <isnendyankp@gmail.com>
2023-09-17 18:22:43 +0700

    S10-89:Add prop data with type array number

--- build/NumberCollection.js
+++ build/NumberCollection.js
@@ -5,3 +5,4 @@ class NumbersCollection {
 }
 exports.NumbersCollection = NumbersCollection;
 // - S10-89:Cr8 base class NumberCollection
+// -S10-89:Add prop data with type array number
--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -1,5 +1,6 @@
 export class NumbersCollection {
-
+    data: number[]
 }
 
 // - S10-89:Cr8 base class NumberCollection
+// -S10-89:Add prop data with type array number
