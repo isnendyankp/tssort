0b9583dd9fd6a050ea6c69ffc5351008cabb0c63
isnendyankp <isnendyankp@gmail.com>
2023-09-17 18:28:41 +0700

    S10-89:Take argument & asssign it too

--- build/NumberCollection.js
+++ build/NumberCollection.js
@@ -3,10 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NumbersCollection = void 0;
 class NumbersCollection {
     constructor(data) {
+        this.data = data;
     }
 }
 exports.NumbersCollection = NumbersCollection;
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
-// - S10-89:Add argument  prop data with type number array @constructor 
+// - S10-89:Add argument  prop data with type number array @constructor
+// - S10-89:Take argument & asssign it too 
--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -2,11 +2,12 @@ export class NumbersCollection {
     data: number[]
 
     constructor(data: number[]) {
-
+        this.data = data;
     }
 }
 
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
-// - S10-89:Add argument  prop data with type number array @constructor 
+// - S10-89:Add argument  prop data with type number array @constructor
+// - S10-89:Take argument & asssign it too 
