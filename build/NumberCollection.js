27f7f93184e60ba545a7f2b5361e81242e599756
isnendyankp <isnendyankp@gmail.com>
2023-09-17 18:26:37 +0700

    S10-89:Add argument  prop data with type number []

--- build/NumberCollection.js
+++ build/NumberCollection.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NumbersCollection = void 0;
 class NumbersCollection {
-    constructor() {
+    constructor(data) {
     }
 }
 exports.NumbersCollection = NumbersCollection;
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
+// - S10-89:Add argument  prop data with type number array @constructor 
--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -1,7 +1,7 @@
 export class NumbersCollection {
     data: number[]
 
-    constructor() {
+    constructor(data: number[]) {
 
     }
 }
@@ -9,3 +9,4 @@ export class NumbersCollection {
 // - S10-89:Cr8 base class NumberCollection
 // -S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
+// - S10-89:Add argument  prop data with type number array @constructor 
