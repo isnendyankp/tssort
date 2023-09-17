0bb59d59ba4fd63ae737c75d5d56cafc0d19115b
isnendyankp <isnendyankp@gmail.com>
2023-09-17 18:30:51 +0700

    S10-89:modifier data & removing assignment

--- build/NumberCollection.js
+++ build/NumberCollection.js
@@ -8,7 +8,8 @@ class NumbersCollection {
 }
 exports.NumbersCollection = NumbersCollection;
 // - S10-89:Cr8 base class NumberCollection
-// -S10-89:Add prop data with type array number
+// - S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
 // - S10-89:Add argument  prop data with type number array @constructor
-// - S10-89:Take argument & asssign it too 
+// - S10-89:Take argument & asssign it too
+// - S10-89:modifier data & removing assignment 
--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -1,13 +1,12 @@
 export class NumbersCollection {
     data: number[]
 
-    constructor(data: number[]) {
-        this.data = data;
-    }
+    constructor(public data: number[]) {}
 }
 
 // - S10-89:Cr8 base class NumberCollection
-// -S10-89:Add prop data with type array number
+// - S10-89:Add prop data with type array number
 // - S10-89:cr8 base constructor @NumberCollection class
 // - S10-89:Add argument  prop data with type number array @constructor
-// - S10-89:Take argument & asssign it too 
+// - S10-89:Take argument & asssign it too
+// - S10-89:modifier data & removing assignment 
