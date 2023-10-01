246f3433a10054fc9f7659e7b839b56eb37205ef
isnendyankp <isnendyankp@gmail.com>
2023-10-01 12:24:19 +0700

    S10-92:cr8 constructor with prop data with type string

--- build/CharacterCollection.js
+++ build/CharacterCollection.js
@@ -2,7 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CharacterCollection = void 0;
 class CharacterCollection {
+    constructor(data) {
+        this.data = data;
+    }
 }
 exports.CharacterCollection = CharacterCollection;
 // - Progress:
 // - S10-92:Cr8 CharacterCollection class
+// - S10-92:cr8 constructor with prop data with type string
--- src/CharacterCollection.ts
+++ src/CharacterCollection.ts
@@ -1,6 +1,8 @@
 export class CharacterCollection {
-
+    constructor(public data: string) {}
+    
 }
 
 // - Progress:
 // - S10-92:Cr8 CharacterCollection class
+// - S10-92:cr8 constructor with prop data with type string
